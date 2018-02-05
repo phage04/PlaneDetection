1ad01cd2e1adc6f761e7874289a36230e466c4e0
lylejover <lylejover2yahoo.com>
2018-02-05 20:23:09 +0100

    plane expansion

--- PlaneDetection/ViewController.swift
+++ PlaneDetection/ViewController.swift
@@ -20,11 +20,46 @@ class ViewController: UIViewController, ARSCNViewDelegate{
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self
+        
+  
+    }
+    
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        return lavaNode
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
         print("New Flat Surface Detected. New AR Plane Anchor Added.")
     }
-}
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("Updating plane anchor.")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("Removing duplicate plan anchor.")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+    }
+    
 
+}
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
